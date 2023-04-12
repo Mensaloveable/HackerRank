c5ad3d6d3562d93a76f9bc04997c81e8b6dc13f0
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-04-12 05:47:31 +0100

    RepeatedStrings

---
+++ src/main/java/com/loveable/WarmupChallenges/RepeatedStrings.java
@@ -0,0 +1,31 @@
+package com.loveable.WarmupChallenges;
+
+/**
+ * There is a string, s, of lowercase English letters that is repeated infinitely many times. Given an integer, n, find and print the number of letter a's in the first n letters of the infinite string.
+ * <p>
+ * Example
+ * s = 'abcac'
+ * n = 10
+ * <p>
+ * The substring we consider is abcacabcac, the first 10 characters of the infinite string. There are 4 occurrences of a in the substring.
+ * <p>
+ * Function Description
+ * <p>
+ * Complete the repeatedString function in the editor below.
+ * <p>
+ * repeatedString has the following parameter(s):
+ * <p>
+ * s: a string to repeat
+ * n: the number of characters to consider
+ * Returns
+ * <p>
+ * int: the frequency of a in the substring
+ * Input Format
+ * <p>
+ * The first line contains a single string, s.
+ * <p>
+ * The second line contains an integer, n.
+ * */
+
+public class RepeatedStrings {
+}
