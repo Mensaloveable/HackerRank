689faed5b1f5d693091fc899e8e7fc0fe1ba0f0f
Mensaloveable <abdullahi.adedayo@gmail.com>
2023-04-15 13:12:02 +0100

    JumpingOnTheClouds

--- src/main/java/com/loveable/WarmupChallenges/JumpingOnTheClouds.java
+++ src/main/java/com/loveable/WarmupChallenges/JumpingOnTheClouds.java
@@ -1,4 +1,5 @@
 package com.loveable.WarmupChallenges;
+import java.util.List;
 
 /**
  * There is a new mobile game that starts with consecutively numbered clouds. Some of the clouds are thunderheads and others are cumulus. The player can jump on any cumulus cloud having a number that is equal to the number of the current cloud plus 1 or 2. The player must avoid the thunderheads. Determine the minimum number of jumps it will take to jump from the starting postion to the last cloud. It is always possible to win the game.
@@ -28,7 +29,22 @@ package com.loveable.WarmupChallenges;
  * <p>
  * The second line contains n space-separated binary integers
  * describing clouds c[I] where 0 <= I < n.
- * */
+ */
 
 public class JumpingOnTheClouds {
+    public static int jumpingOnClouds(List<Integer> c) {
+        int jumps = 0;
+        int i = 0;
+        while (i < c.size() - 1) {
+            if (i + 2 < c.size() && c.get(i + 2) == 0) {
+                // Jumping two clouds is possible
+                i += 2;
+            } else {
+                // Jumping one cloud is the only option
+                i += 1;
+            }
+            jumps++;
+        }
+        return jumps;
+    }
 }
